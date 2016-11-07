6c63dee74d77c477022cef3f5b2053b57bf90fa9
Guy Waguespack <guy.waguespack@bp.com>
2016-11-07 13:32:42 -0600

    Adding CommandLineHandler class.

---
+++ Synapse.Handler.CommandLine/CommandLineHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+using Synapse.Core;
+
+namespace Synapse.Handler.CommandLine
+{
+    public class CommandLineHandler : HandlerRuntimeBase
+    {
+        override public ExecuteResult Execute(HandlerStartInfo startInfo)
+        {
+            Console.WriteLine(startInfo.Parameters);
+            return new ExecuteResult() { Status = StatusType.Complete };
+        }
+    }
+}
